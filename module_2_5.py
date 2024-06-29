e6f2ae15696e79575633e64d8ce2df12775755e3
Sergey13131313 <110328066+Sergey13131313@users.noreply.github.com>
2024-06-29 07:42:52 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,32 @@
+# Напишите функцию get_matrix с тремя параметрами n, m и value,
+# которая будет создавать матрицу(вложенный список) размерами n
+# строк и m столбцов, заполненную значениями value и возвращать
+# эту матрицу в качестве результата работы.
+
+def showMatrix(matrix):
+    typeTemplate = []
+    if type(matrix) != type(typeTemplate):
+        print('Неверный тип аргумента!')
+        return
+    else:
+        for i in matrix:
+            for j in range(len(i)):
+                print(i[j], end = ' ')
+            print('')
+def returnList(count, value):
+    tempList = []
+    for i in range(count):
+        tempList.append(value)
+    return tempList
+
+def getMatrix(n, m, value):
+    matrixList = []
+    for i in range(n):
+        matrixList.append(returnList(m, value))
+    return matrixList
+
+matrix = getMatrix(3, 3, 9)
+print(matrix)
+showMatrix(matrix)
+showMatrix(5)
+
